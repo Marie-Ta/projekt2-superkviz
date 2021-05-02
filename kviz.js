ecae0411e59d7506015645c513dde44e2aa1d54a
Marie Tarantova <marie.tarantova@gmail.com>
2021-05-02 19:41:41 +0200

    Základní zobrazení nastaveno - prozatím nesprávná čísla otázek a nefunkční zobrazování možností

---
+++ kviz.js
@@ -0,0 +1,50 @@
+/*Data pro kvíz jsou uložena v poli otazky. 
+Každa otázka je objekt, který obsahuje otázku, obrázek k otázce, pole možných odpovědí a index správné odpovědi.*/
+
+let otazky = [
+    {   otazka: "Kdo slaví svátek 2. května?",
+        obrazek: "obrazky/pivo.jpg",
+        moznosti: ["Karel",  "Zikmund", "Václav"],
+        spravneIndex: 1
+    },
+
+    {   otazka: "Které ovoce připomíná svým vzhledem ostružinu?",
+        obrazek: "obrazky/ovoce.jpg",
+        moznosti: ["Mouruše", "Moduše", "Moruše"],
+        spravneIndex: 2
+    }
+    
+];
+
+//Zobrazení čísla otázky
+let poradi = document.querySelector("#poradi");
+poradi.textContent = 'Otázka '+ otazky.indexOf('otazka') + ' / ' + otazky.length;
+
+
+// Zobrazení otázky na stránce
+let otazka = document.querySelector("#otazka");
+otazka.textContent = otazky[0].otazka;
+
+// Zobrazení obrázku u otázky na stránce
+let obrazek = document.querySelector("#obrazek");
+obrazek.src = otazky[0].obrazek;
+
+// Zobrazení možných odpovědí na otázku na stránce
+function pridejOdpoved(i){ 
+let novaOdpoved = document.createElement('li');
+let seznamOdpovedi = document.querySelector('ul');
+seznamOdpovedi.appendChild(novaOdpoved);
+
+novaOdpoved.textContent = otazky[0].moznosti[0];
+}
+
+otazky.forEach(pridejOdpoved)
+
+
+
+
+
+
+
+
+
