7ec677f51b9225b23e9f05bb30a390818f49bcd9
Marie Tarantova <marie.tarantova@gmail.com>
2021-05-06 19:03:29 +0200

    Vypisování Otázky a vybrané odpovědi do konzole

--- kviz.js
+++ kviz.js
@@ -26,7 +26,7 @@ let y = 0;
 let obrazek = document.querySelector("#obrazek");
 let vysledek = document.querySelector('.vysledek');
 let otazka = document.querySelector("#otazka");
-
+let vyhodnoceni = document.querySelector("#vyhodnoceni");
 
 // Zobrazení otázky na stránce
 function zobrazOtazku(i){
@@ -34,39 +34,50 @@ function zobrazOtazku(i){
 
     poradi.textContent = 'Otázka '+ (y+1) + ' / ' + otazky.length;
     odpovedi.innerHTML = "";
-  
+   
     otazka.textContent = otazky[i].otazka;
     obrazek.src = otazky[y].obrazek;
-    
+      
     for (let i=0; i<otazky[y].moznosti.length; i=i+1) { 
         let novaOdpoved = document.createElement('li');
         let seznamOdpovedi = document.querySelector('ul');
         
         seznamOdpovedi.appendChild(novaOdpoved);
         novaOdpoved.textContent = otazky[y].moznosti[i];
+   
+        novaOdpoved.addEventListener('click', function(){
         
+        //zobrazení otázky a vybrané odpovědi v konzoli
+        console.log((y+1) + '. ' + otazka.textContent),
+        console.log('Tvoje odpověď: ' + novaOdpoved.innerText)
+        
+        //vyhodnoceni.textContent = otazka.textContent
+       });
     }
 }
 
 zobrazOtazku(i);
 
 // Přepnutí na další otázku
-let moznost = document.querySelector("ul"); //je potřeba, aby slyšel, na které jednotlivé li je kliknuto, ne jen ul
-
+let moznost = document.querySelector("ul");
 
 moznost.addEventListener('click', function(){
     y=y+1;
     i=i+1;
-
+    
     if (y<otazky.length){
-        zobrazOtazku(i);
+        zobrazOtazku(i);  
+                 
     }
 
     else{
         zobrazHodnoceni();
     }
-}) 
 
+  // let presunutaOtazka = event.target;
+  //  console.log(presunutaOtazka)
+  
+}) 
 
 function zobrazHodnoceni(){
     
@@ -76,14 +87,11 @@ function zobrazHodnoceni(){
     kviz.style.display='none';
 }
 
+//vysledek.innerHTML = 'otazka'
 
-
-
-let varianta = document.querySelector('li');
+/*let varianta = document.querySelector('li');
 
 varianta.addEventListener('click', function(){
-    console.log(varianta);
+    console.log(varianta.innerText);
 })
-
-
-//vysledek.appendChild(otazka);
+*/
